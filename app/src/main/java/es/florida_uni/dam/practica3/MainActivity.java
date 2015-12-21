53c1b585f9c2991df8877f3347ac978259ed9137
Alvaro Maceda <alvaro@alvaromaceda.es>
2015-12-21 10:19:04 +0100

    Propuesta final de la práctica

--- app/src/main/java/es/florida_uni/dam/practica3/MainActivity.java
+++ app/src/main/java/es/florida_uni/dam/practica3/MainActivity.java
@@ -1,10 +1,13 @@
 package es.florida_uni.dam.practica3;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +15,38 @@ public class MainActivity extends AppCompatActivity {
     final static String TAG = "SYP-practica3";
 
     TareaMuyCostosa tareaMuyCostosa;
+    public ProgressBar barraProgreso;
+    ReceptorMensajes handler = new ReceptorMensajes();
+
+
+    // TODO: Programar el handler para recibir el mensaje de finalización y actuar en consecuencia
+    // Tendrá que llamar a la función tareaMuyCostosaFinalizada()
+    //
+    // OJO: para aplicaciones reales habría que hacer el código un poco más complejo, ver:
+    // http://stackoverflow.com/questions/11407943/this-handler-class-should-be-static-or-leaks-might-occur-incominghandler
+    // http://www.intertech.com/Blog/android-non-ui-to-ui-thread-communications-part-3-of-5/
+    // No uses esto directamente en una aplicación real
+    class ReceptorMensajes extends Handler {
+        @Override
+        public void handleMessage(Message mensaje) {
+            super.handleMessage(mensaje);
+        }
+    }
+
+    private void tareaMuyCostosaFinalizada(){
+        // Aqui podríamos actualizar el interface de usuario, etc.
+        Log.i(TAG, "Tratamiento del mensaje de finalización de la tarea muy costosa");
+        tareaMuyCostosa = null;
+        Toast.makeText(
+                this, "¡Tarea muy costosa finalizada!", Toast.LENGTH_SHORT
+                ).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        barraProgreso = (ProgressBar) findViewById(R.id.barraProgreso);
 
         instalarManejadores();
     }
@@ -64,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        tareaMuyCostosa = new TareaMuyCostosa();
+        tareaMuyCostosa = new TareaMuyCostosa(this,handler);
         new Thread(tareaMuyCostosa).start();
     }
 
